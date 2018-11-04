edb36f2ba12053963bed0c96cd5c505654f00eda
Reed20150104 <935667006@qq.com>
2018-11-04 23:54:20 +0800

    add some structure files

---
+++ 0000/structureofmine.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import PIL
+from PIL import Image,ImageDraw,ImageFont
+
+
+'''
+ 此文件为文件结构说明，包括python解释器说明，文档编码方式，文件功能说明，主函数入口。
+ python文件功能说明在此处
+'''
+
+def sayHello():
+    print('Hello world')
+
+if __name__ == '__main__':
+    sayHello()
---
+++ structureofmine.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+'''
+ 此文件为文件结构说明，包括python解释器说明，文档编码方式，文件功能说明，主函数入口。
+ python文件功能说明在此处
+'''
+
+def sayHello():
+    print('Hello world')
+
+if __name__ == '__main__':
+    sayHello()
