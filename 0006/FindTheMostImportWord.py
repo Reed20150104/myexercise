55400dd9bd10e0e544830c0c6fc6f4ede8d6f7d7
Reed20150104 <935667006@qq.com>
2019-02-13 22:41:14 +0800

    task8 accomplished!

---
+++ 0006/FindTheMostImportWord.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# import lib
+
+'''
+Project: myexercise
+File: FindTheMostImportWord.py
+Author: Reed
+Date: 2019/1/16 22:30
+
+'''
+
---
+++ 0007/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# import lib
+
+'''
+Project: myexercise
+File: test.py
+Author: Reed
+Date: 2019/2/13 22:17
+
+'''
+
