31ce9f8737f96d2f65acccb383d7cb73d086513d
Reed20150104 <935667006@qq.com>
2019-02-13 22:41:26 +0800

    task8 accomplished!

--- 0006/FindTheMostImportWord.py
+++ 0006/FindTheMostImportWord.py
@@ -7,6 +7,8 @@
 Project: myexercise
 File: FindTheMostImportWord.py
 Author: Reed
+这里的最重要的词，应该是指出现次数最多的词，其实与0004中的问题比较类似
+再多一步排序即可。
 Date: 2019/1/16 22:30
 
 '''
--- 0007/test.py
+++ 0007/test.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+def gci(filepath):
+#遍历filepath下所有文件，包括子目录
+  files = os.listdir(filepath)
+  for fi in files:
+    fi_d = os.path.join(filepath,fi)
+    if os.path.isdir(fi_d):
+      gci(fi_d)
+    else:
+      print(os.path.join(filepath,fi_d))
 
-# import lib
-
-'''
-Project: myexercise
-File: test.py
-Author: Reed
-Date: 2019/2/13 22:17
-
-'''
+#递归遍历/root目录下所有文件
+gci('')
 
