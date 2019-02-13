e66d30a5115c975bbca612a5085ea0287dfa8649
Reed20150104 <935667006@qq.com>
2019-02-13 22:41:26 +0800

    task8 accomplished!

--- 0005/dealWithPictures.py
+++ 0005/dealWithPictures.py
@@ -5,48 +5,46 @@
 import os
 from PIL import Image
 
-
 '''
 Project: myexercise
 File: dealWithPictures.py
 Comment: This python file is used to deal with picture size.
-Author: Reed   https://www.cnblogs.com/dangzhengtao/p/9620272.html  https://www.jb51.net/article/138966.htm
+Author: Reed   
 Date: 2019/1/9 23:20
 
 '''
 
 
-def changePictureSize(filePath,width,height,distPath):
+def changePictureSize(filePath, width, height, distPath):
+    ext = ['jpg', 'png', 'jpeg']  # 需要处理的图片文件类型
     for root, dirs, files in os.walk(filePath):
         for file in files:
-            image = Image.open(os.path.join(filePath, file))
-            image_width = image.width
-            image_height = image.height
+            if file.split('.')[-1] in ext:
+                image = Image.open(os.path.join(filePath, file))
+                image_width = image.width
+                image_height = image.height
 
-            if image_width <= width and image_height <= height:
-                print(file, " is OK")
-                return
-            if 1.0 * image_width / width > 1.0 * image_height / height:
-                scale = 1.0 * image_width / width
-                new_image = image.resize((int(image_width / scale), int(image_height / scale)))
-            else:
-                scale = 1.0 * image_height / height
-                new_image = image.resize((int)(image_width / scale), int(image_height / scale), Image.ANTIALIAS)
+                if image_width <= width and image_height <= height:
+                    print(file, " is OK")
+                    return
+                if 1.0 * image_width / width > 1.0 * image_height / height:
+                    scale = 1.0 * image_width / width
+                    new_image = image.resize((int(image_width / scale), int(image_height / scale)))
+                else:
+                    scale = 1.0 * image_height / height
+                    new_image = image.resize((int)(image_width / scale), int(image_height / scale), Image.ANTIALIAS)
 
-            if not os.path.exists(distPath):
-                os.makedirs(distPath)
+                if not os.path.exists(distPath):
+                    os.makedirs(distPath)
 
-            new_image.save(os.path.join(distPath, 'new--' + file))
-            new_image.close()
-            image.close()
-
-
+                new_image.save(os.path.join(distPath, 'new--' + file))
+                new_image.close()
+                image.close()
 
 
 def main():
-    FILEPATH='E:\\BaiduDownloads\\191_1222'  #图片文件路径
-    DIST_PATH = 'E:\\pictureResult'  #存放图片路径
-    ext = ['jpg','png','jpeg']   #需要处理的图片文件类型
+    FILEPATH = ''  # 图片文件路径
+    DIST_PATH = ''  # 存放图片路径
     PHONE_WIDTH = 640
     PHONE_HEIGHT = 1136
     changePictureSize(FILEPATH, 640, 1136, DIST_PATH)
--- 0007/statisticalCode.py
+++ 0007/statisticalCode.py
@@ -2,12 +2,79 @@
 # -*- encoding: utf-8 -*-
 
 # import lib
+import os
 
 '''
 Project: myexercise
 File: statisticalCode.py
-Author: Reed
+Comment: 
+Author: Reed https://www.cnblogs.com/hupeng1234/p/6680230.html
 Date: 2019/1/16 22:35
 
 '''
 
+def getAllFiles(filePath):
+    fileArr = []
+    for root,dir,files in os.walk(filePath):
+        for file in files:
+            fileArr.append(os.path.join(root,file))
+    # files = os.listdir(filePath)
+    # for file in files:
+    #     file_dir = os.path.join(filePath,file)
+    #     if os.path.isdir(file_dir):
+    #         getAllFiles(file_dir)
+    #     else:
+    #         fileArr.append(os.path.join(filePath,file_dir))
+    #         print(os.path.join(filePath,file_dir))
+
+    return fileArr
+
+def staticalCode(filePath):
+    DICTSTATICAL = {}
+    FILE_ARRAY = []
+    CODE_NUMBER= 0   #代码行数
+    COMMENT_NUMBER = 0 #注释行数
+    BLANK_NUMBER = 0  #空白行数
+    IS_COMMENT = False  #默认非注释行
+    FILE_ARRAY = getAllFiles(filePath)
+    print(len(FILE_ARRAY))
+    for file in FILE_ARRAY:
+        if file.endswith('.py'):
+            with open(file, 'r', encoding='utf-8') as pyfile:
+                for index, line in enumerate(pyfile, start=1):
+                    line = line.strip()
+
+                    if not IS_COMMENT:
+                        if line.startswith("'''") or line.startswith('"""'):
+                            IS_COMMENT = True
+                            START_COMMENT_INDEX = index
+
+                        elif line.startswith('#'):
+                            COMMENT_NUMBER += 1
+                        elif line == '':
+                            BLANK_NUMBER += 1
+                        else:
+                            CODE_NUMBER += 1
+
+                    else:
+                        if line.endswith("'''") or line.endswith('"""'):
+                            IS_COMMENT = False
+                            COMMENT_NUMBER += index - START_COMMENT_INDEX + 1
+                        else:
+                            pass
+        else:
+            pass
+
+
+    DICTSTATICAL = {'CODE_NUMBER': CODE_NUMBER,'COMMENT_NUMBER':COMMENT_NUMBER, 'BLANK_NUMBER':BLANK_NUMBER}
+    return DICTSTATICAL
+
+def main():
+    TOTAL_ALL={}
+    MYCODEDIR=''
+    TOTAL_ALL = staticalCode(MYCODEDIR)
+    print(TOTAL_ALL)
+
+
+if __name__ == '__main__':
+    main()
